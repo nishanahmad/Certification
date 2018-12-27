b66c79a4c1a5817c63da9b746796810c72e1051b
Nishan Ahmed <nishanahmad@gmail.com>
2018-12-27 16:36:43 +0530

    first commit

--- user_login_session.php
+++ user_login_session.php
@@ -6,14 +6,13 @@ require 'connect.php';
 session_start();
 if(count($_POST)>0) 
 {
-	$result = mysqli_query($con,"SELECT * FROM users WHERE user_name='" . $_POST["user_name"] . "' and password = '". $_POST["password"]."'") or die(mysqli_error($con));				 
+	$result = mysqli_query($con,"SELECT * FROM users WHERE name='" . $_POST["user_name"] . "' and password = '". $_POST["password"]."'") or die(mysqli_error($con));				 
 	$row_cnt = $result->num_rows;
 	if($row_cnt > 0)
 	{
 		$user  = mysqli_fetch_array($result, MYSQLI_ASSOC);
-		$_SESSION['user_id'] = $user['user_id'];
-		$_SESSION['user_name'] = $user['user_name'];
-		$_SESSION['role'] = $user['role'];		
+		$_SESSION['id'] = $user['id'];
+		$_SESSION['name'] = $user['name'];
 		header("Location:index.php");
 	}
 	else 
